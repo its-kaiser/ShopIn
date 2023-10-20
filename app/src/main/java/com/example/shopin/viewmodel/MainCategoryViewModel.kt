488156dbbb84fd32b907899836bcafe0c5c3629f
Himanshu Arya <stararya1234@gmail.com>
2023-10-20 17:32:04 +0530

    changes in the maincategoryfragment

---
+++ app/src/main/java/com/example/shopin/viewmodel/MainCategoryViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.shopin.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.shopin.data.Product
+import com.example.shopin.utils.Resource
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+class MainCategoryViewModel @Inject constructor(
+    private val firestore:FirebaseFirestore
+):ViewModel() {
+
+    private val _specialProducts  = MutableStateFlow<Resource<List<Product>>>(Resource.Unspecified())
+    val specialProducts: StateFlow<Resource<List<Product>>> = _specialProducts
+
+    fun fetchSpecialProducts(){
+        firestore.collection("Products").whereEqualTo("category","Special Products")
+    }
+}
