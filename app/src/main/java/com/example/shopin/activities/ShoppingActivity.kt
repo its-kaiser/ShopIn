80bf1be9789d7a28bcd568db778c22c26855e6d4
Himanshu Arya <stararya1234@gmail.com>
2023-10-23 18:41:02 +0530

    fetching special products

--- app/src/main/java/com/example/shopin/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/shopin/activities/ShoppingActivity.kt
@@ -3,7 +3,9 @@ package com.example.shopin.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.shopin.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ShoppingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/shopin/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/com/example/shopin/fragments/categories/MainCategoryFragment.kt
@@ -1,19 +1,33 @@
 package com.example.shopin.fragments.categories
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shopin.R
 import com.example.shopin.adapters.SpecialProductsAdapter
 import com.example.shopin.databinding.FragmentMainCategoryBinding
+import com.example.shopin.utils.Resource
+import com.example.shopin.viewmodel.MainCategoryViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class MainCategoryFragment:Fragment(R.layout.fragment_main_category) {
 
+    companion object{
+        private const val TAG="MainCategoryFragment"
+    }
     private lateinit var binding : FragmentMainCategoryBinding
     private lateinit var specialProductsAdapter: SpecialProductsAdapter
+
+    private val viewModel by viewModels<MainCategoryViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,6 +41,34 @@ class MainCategoryFragment:Fragment(R.layout.fragment_main_category) {
         super.onViewCreated(view, savedInstanceState)
 
         setupSpecialProductsRv()
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.specialProducts.collectLatest {
+                when(it){
+                    is Resource.Loading->{
+                        showLoading()
+                    }
+                    is Resource.Success->{
+                        specialProductsAdapter.differ.submitList(it.data)
+                        hideLoading()
+                    }
+                    is Resource.Error->{
+                        hideLoading()
+                        Log.e(TAG,it.message.toString())
+                        Toast.makeText(requireContext(),it.message,Toast.LENGTH_SHORT).show()
+                    }
+                    else->Unit
+                }
+            }
+        }
+    }
+
+    private fun hideLoading() {
+        binding.pbMainCategoryProgressBar.visibility=View.GONE
+    }
+
+    private fun showLoading() {
+        binding.pbMainCategoryProgressBar.visibility=View.VISIBLE
     }
 
     private fun setupSpecialProductsRv() {
