502504833365b48f7c06dc802c726a7edb2bc6bc
Himanshu Arya <stararya1234@gmail.com>
2023-10-29 01:59:50 +0530

    address fragment setup

--- app/src/main/java/com/example/shopin/fragments/shopping/AddressFragment.kt
+++ app/src/main/java/com/example/shopin/fragments/shopping/AddressFragment.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.shopin.data.Address
 import com.example.shopin.databinding.FragmentAddressBinding
+import com.example.shopin.utils.Resource
+import com.example.shopin.viewmodel.AddressViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class AddressFragment:Fragment() {
 
     private lateinit var binding: FragmentAddressBinding
-
+    val viewModel by viewModels<AddressViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,7 +30,49 @@ class AddressFragment:Fragment() {
         return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.addNewAddress.collectLatest {
+                when(it){
+                    is Resource.Loading->{
+                        binding.progressbarAddress.visibility = View.VISIBLE
+                    }
+                    is Resource.Success->{
+                         binding.progressbarAddress.visibility = View.INVISIBLE
+                        findNavController().navigateUp()
+                    }
+                    is Resource.Error->{
+                        Toast.makeText(requireContext(),it.message,Toast.LENGTH_SHORT).show()
+                    }
+                    else->Unit
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.error.collectLatest {
+                Toast.makeText(requireContext(),it,Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            btnSave.setOnClickListener{
+                val addressTitle =edAddressTitle.text.toString()
+                val fullName = edFullName.text.toString()
+                val street = edStreet.text.toString()
+                val city = edCity.text.toString()
+                val phoneNumber = edPhone.text.toString()
+                val state = edState.text.toString()
+                val address = Address(addressTitle,fullName,street,phoneNumber,city,state)
+
+                viewModel.addAddress(address)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/shopin/viewmodel/AddressViewModel.kt
+++ app/src/main/java/com/example/shopin/viewmodel/AddressViewModel.kt
@@ -1,17 +1,61 @@
 package com.example.shopin.viewmodel
 
-import android.location.Address
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.shopin.data.Address
+import com.example.shopin.utils.Resource
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AddressViewModel @Inject constructor(
-    private val firestore: FirebaseFirestore
+    private val firestore: FirebaseFirestore,
+    private val fbAuth: FirebaseAuth
 ):ViewModel() {
 
-    fun addAddress(address: Address){
+    private val _addNewAddress = MutableStateFlow<Resource<Address>>(Resource.Unspecified())
+    val addNewAddress = _addNewAddress.asStateFlow()
 
+    private val _error = MutableSharedFlow<String>()
+    val error = _error.asSharedFlow()
+    fun addAddress(address: Address){
+        val valid = validateInputs(address)
+        if(valid) {
+            viewModelScope.launch {
+                _addNewAddress.emit(Resource.Loading())
+            }
+            firestore.collection("user").document(fbAuth.uid!!).collection("address")
+                .document().set(address)
+                .addOnSuccessListener {
+                    viewModelScope.launch {
+                        _addNewAddress.emit(Resource.Success(address))
+                    }
+                }.addOnFailureListener {
+                    viewModelScope.launch {
+                        _addNewAddress.emit(Resource.Error(it.message.toString()))
+                    }
+                }
+        }
+        else{
+            viewModelScope.launch {
+                _error.emit("All fields are required!")
+            }
+        }
+    }
+
+    private fun validateInputs(address: Address): Boolean {
+        return address.addressTitle.trim().isNotEmpty() &&
+                address.state.trim().isNotEmpty() &&
+                address.fullName.trim().isNotEmpty() &&
+                address.city.trim().isNotEmpty() &&
+                address.fullName.trim().isNotEmpty() &&
+                address.street.trim().isNotEmpty()
     }
 }
