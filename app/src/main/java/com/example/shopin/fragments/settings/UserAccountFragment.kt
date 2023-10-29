c027a1eaa65a20fa4c1caf3a9f909a7e42c90395
Himanshu Arya <stararya1234@gmail.com>
2023-10-30 04:46:32 +0530

    user account fragment setup

--- app/src/main/java/com/example/shopin/fragments/settings/UserAccountFragment.kt
+++ app/src/main/java/com/example/shopin/fragments/settings/UserAccountFragment.kt
@@ -1,4 +1,166 @@
 package com.example.shopin.fragments.settings
 
-class UserAccountFragment {
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.example.shopin.data.User
+import com.example.shopin.databinding.FragmentUserAccountBinding
+import com.example.shopin.dialog.setupBottomSheetDialog
+import com.example.shopin.utils.Resource
+import com.example.shopin.viewmodel.UserAccountViewModel
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+
+@AndroidEntryPoint
+class UserAccountFragment: Fragment() {
+
+    private lateinit var binding:FragmentUserAccountBinding
+    private val viewModel by viewModels<UserAccountViewModel>()
+    private lateinit var imageActivityLauncher :ActivityResultLauncher<Intent>
+    private var imageUri : Uri? =null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        imageActivityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            imageUri =it.data?.data
+            Glide.with(this).load(imageUri).into(binding.ivUser)
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentUserAccountBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.user.collectLatest {
+                when(it){
+                    is Resource.Loading->{
+                        showUserLoading()
+                    }
+                    is Resource.Success->{
+                        hideUserLoading()
+                        showUserInfo(it.data!!)
+                    }
+                    is Resource.Error->{
+                        hideUserLoading()
+                    }
+                    else->Unit
+                }
+            }
+        }
+        lifecycleScope.launchWhenStarted {
+            viewModel.updateInfo.collectLatest {
+                when(it){
+                    is Resource.Loading->{
+                        binding.btnSaveAccount.startAnimation()
+                    }
+                    is Resource.Success->{
+                        binding.btnSaveAccount.revertAnimation()
+                        findNavController().navigateUp()
+                    }
+                    is Resource.Error->{
+                        binding.btnSaveAccount.revertAnimation()
+                    }
+                    else->Unit
+                }
+            }
+        }
+
+        binding.btnSaveAccount.setOnClickListener {
+            binding.apply {
+                val firstName = etFirstName.text.trim().toString()
+                val lastName = etLastName.text.trim().toString()
+                val email = etEmailAccount.text.trim().toString()
+
+                val user = User(firstName, lastName, email)
+                viewModel.updateUserInfo(user, imageUri)
+            }
+        }
+
+        binding.ivEdit.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type= "image/*"
+            imageActivityLauncher.launch(intent)
+        }
+
+        binding.tvUpdatePassword.setOnClickListener {
+            setupBottomSheetDialog {email->
+                viewModel.resetPassword(email)
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.resetPassword.collect{
+                when(it){
+                    is Resource.Loading ->{
+
+                    }
+                    is Resource.Error ->{
+                        Snackbar.make(requireView(), "Error: ${it.message}", Snackbar.LENGTH_LONG).show()
+                    }
+                    is Resource.Success->{
+                        Snackbar.make(requireView(), "Reset link has been sent to your email", Snackbar.LENGTH_LONG).show()
+                    }
+                    else->Unit
+                }
+            }
+        }
+    }
+
+    private fun showUserInfo(data: User) {
+        binding.apply {
+            Glide.with(this@UserAccountFragment).load(data.imagePath).error(ColorDrawable(Color.BLACK)).into(ivUser)
+            etFirstName.setText(data.firstName)
+            etLastName.setText(data.lastName)
+            etEmailAccount.setText(data.email)
+        }
+    }
+
+    private fun hideUserLoading() {
+        binding.apply {
+            progressbarAccount.visibility = View.GONE
+            ivUser.visibility= View.VISIBLE
+            ivEdit.visibility= View.VISIBLE
+            etFirstName.visibility= View.VISIBLE
+            etLastName.visibility= View.VISIBLE
+            etEmailAccount.visibility= View.VISIBLE
+            tvUpdatePassword.visibility= View.VISIBLE
+            btnSaveAccount.visibility= View.VISIBLE
+        }
+    }
+
+    private fun showUserLoading() {
+        binding.apply {
+            progressbarAccount.visibility = View.VISIBLE
+            ivUser.visibility= View.INVISIBLE
+            ivEdit.visibility= View.INVISIBLE
+            etFirstName.visibility= View.INVISIBLE
+            etLastName.visibility= View.INVISIBLE
+            etEmailAccount.visibility= View.INVISIBLE
+            tvUpdatePassword.visibility= View.INVISIBLE
+            btnSaveAccount.visibility= View.INVISIBLE
+        }
+    }
 }
--- app/src/main/java/com/example/shopin/viewmodel/UserAccountViewModel.kt
+++ app/src/main/java/com/example/shopin/viewmodel/UserAccountViewModel.kt
@@ -1,4 +1,151 @@
 package com.example.shopin.viewmodel
 
-class UserAccountViewModel {
+import android.app.Application
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.shopin.ShopInApplication
+import com.example.shopin.data.User
+import com.example.shopin.utils.RegisterValidation
+import com.example.shopin.utils.Resource
+import com.example.shopin.utils.validateEmail
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.StorageReference
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.io.ByteArrayOutputStream
+import java.util.UUID
+import javax.inject.Inject
+
+@HiltViewModel
+class UserAccountViewModel @Inject constructor(
+    private val firestore: FirebaseFirestore,
+    private val fbAuth: FirebaseAuth,
+    private val storage:StorageReference,
+    app:Application
+): AndroidViewModel(app){
+
+    private val _user = MutableStateFlow<Resource<User>>(Resource.Unspecified())
+    val user = _user.asStateFlow()
+
+    private val _updateInfo = MutableStateFlow<Resource<User>>(Resource.Unspecified())
+    val updateInfo = _updateInfo.asStateFlow()
+
+    private val _resetPassword = MutableSharedFlow<Resource<String>>()
+    val resetPassword = _resetPassword.asSharedFlow()
+
+    init {
+        fetchUser()
+    }
+    fun fetchUser(){
+        viewModelScope.launch {
+            _user.emit(Resource.Loading())
+        }
+
+        firestore.collection("user").document(fbAuth.uid!!).get()
+            .addOnSuccessListener {
+                val user = it.toObject(User::class.java)
+                user?.let{
+                    viewModelScope.launch {
+                        _user.emit(Resource.Success(it))
+                    }
+                }
+            }.addOnFailureListener{
+                viewModelScope.launch {
+                    _user.emit(Resource.Error(it.message.toString()))
+                }
+            }
+    }
+
+    fun updateUserInfo(user :User, imageUri: Uri?){
+        val valid = validateEmail(user.email) is RegisterValidation.Success
+                && user.firstName.trim().isNotEmpty()
+                && user.lastName.trim().isNotEmpty()
+
+        if(!valid){
+            viewModelScope.launch {
+                _user.emit(Resource.Error("Check your inputs"))
+            }
+            return
+        }
+        viewModelScope.launch {
+            _updateInfo.emit(Resource.Loading())
+        }
+
+        if(imageUri==null){
+            saveUserInfo(user,true)
+        }else{
+            saveUserInfoWithNewImage(user,imageUri)
+        }
+    }
+
+    private fun saveUserInfoWithNewImage(user: User, imageUri: Uri) {
+        viewModelScope.launch {
+            try {
+                val imageBitmap = MediaStore.Images.Media.getBitmap(
+                    getApplication<ShopInApplication>().contentResolver, imageUri)
+
+                val byteArrayOutputStream = ByteArrayOutputStream()
+                imageBitmap.compress(Bitmap.CompressFormat.JPEG,96,byteArrayOutputStream)
+                val imageByteArray = byteArrayOutputStream.toByteArray()
+                val imageDirectory = storage.child("profileImages/${fbAuth.uid}/${UUID.randomUUID()}")
+
+                val result = imageDirectory.putBytes(imageByteArray).await()
+                val imageUrl = result.storage.downloadUrl.await().toString()
+                saveUserInfo(user.copy(imagePath = imageUrl),false)
+            }catch (e:Exception){
+                viewModelScope.launch {
+                    _updateInfo.emit(Resource.Error(e.message.toString()))
+                }
+            }
+        }
+    }
+
+    private fun saveUserInfo(user: User, shouldRetrieveOldImage: Boolean) {
+
+        firestore.runTransaction{transaction->
+            val docRef = firestore.collection("user").document(fbAuth.uid!!)
+
+            if(shouldRetrieveOldImage){
+                val currentUser = transaction.get(docRef).toObject(User::class.java)
+                val newUser = user.copy(imagePath = currentUser?.imagePath?: "")
+                transaction.set(docRef,newUser)
+            }
+            else{
+                transaction.set(docRef,user)
+            }
+        }.addOnSuccessListener {
+            viewModelScope.launch {
+                _updateInfo.emit(Resource.Success(user))
+            }
+        }.addOnFailureListener{
+            viewModelScope.launch {
+                _updateInfo.emit(Resource.Error(it.message.toString()))
+            }
+        }
+    }
+
+    fun resetPassword(email: String){
+        viewModelScope.launch {
+            _resetPassword.emit(Resource.Loading())
+        }
+        fbAuth.sendPasswordResetEmail(email)
+            .addOnSuccessListener {
+                viewModelScope.launch {
+                    _resetPassword.emit(Resource.Success(email))
+                }
+            }.addOnFailureListener{
+                viewModelScope.launch {
+                    _resetPassword.emit(Resource.Error(it.message.toString()))
+                }
+            }
+    }
 }
