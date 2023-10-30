61cd1ab924f82d8c0f6504b1f8356013666012d6
Himanshu Arya <stararya1234@gmail.com>
2023-10-31 01:39:02 +0530

    profile fragment setup

--- app/src/main/java/com/example/shopin/fragments/shopping/ProfileFragment.kt
+++ app/src/main/java/com/example/shopin/fragments/shopping/ProfileFragment.kt
@@ -1,7 +1,87 @@
 package com.example.shopin.fragments.shopping
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.shopin.R
+import com.example.shopin.activities.LoginRegisterActivity
+import com.example.shopin.databinding.FragmentProfileBinding
+import com.example.shopin.utils.Resource
+import com.example.shopin.utils.showBottomNavigatioView
+import com.example.shopin.viewmodel.ProfileViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class ProfileFragment:Fragment(R.layout.fragment_profile) {
+
+    private lateinit var binding: FragmentProfileBinding
+    private val viewModel by viewModels<ProfileViewModel> ()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.constraintProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_userAccountFragment)
+        }
+
+        binding.linearAllOrders.setOnClickListener{
+            findNavController().navigate(R.id.action_profileFragment_to_ordersFragment)
+        }
+
+        binding.linearBilling.setOnClickListener {
+            val action = ProfileFragmentDirections.actionProfileFragmentToBillingFragment(0f,
+                emptyArray())
+
+            findNavController().navigate(action)
+        }
+
+        binding.linearLogOut.setOnClickListener {
+            viewModel.logOut()
+            val intent = Intent(requireActivity(),LoginRegisterActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.user.collectLatest {
+                when(it){
+                    is Resource.Loading->{
+                        binding.progressbarSettings.visibility = View.VISIBLE
+                    }
+                    is Resource.Success->{
+                        binding.progressbarSettings.visibility = View.GONE
+                        Glide.with(requireView()).load(it.data!!.imagePath).error(R.drawable.ic_empty_dp).into(binding.ivUserProfile)
+                        binding.tvUserName.text = "${it.data!!.firstName} ${it.data!!.lastName}"
+                    }
+                    is Resource.Error->{
+                        binding.progressbarSettings.visibility = View.GONE
+                    }
+                    else->Unit
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        showBottomNavigatioView()
+    }
 }
--- app/src/main/java/com/example/shopin/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/shopin/viewmodel/ProfileViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.shopin.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.shopin.data.User
 import com.example.shopin.utils.Resource
 import com.google.firebase.auth.FirebaseAuth
@@ -8,6 +9,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,7 +21,32 @@ class ProfileViewModel @Inject constructor(
     private val _user = MutableStateFlow<Resource<User>>(Resource.Unspecified())
     val user = _user.asStateFlow()
 
+    init {
+        fetchUser()
+    }
     fun fetchUser(){
-        firestore.collection("user").ducument
+        viewModelScope.launch {
+            _user.emit(Resource.Loading())
+        }
+        firestore.collection("user").document(fbAuth.uid!!)
+            .addSnapshotListener{value,error->
+                 if(error!=null){
+                     viewModelScope.launch {
+                         _user.emit(Resource.Error(error.message.toString()))
+                     }
+                 }
+                else{
+                    val user = value?.toObject(User::class.java)
+                     user?.let {
+                         viewModelScope.launch {
+                             _user.emit(Resource.Success(user))
+                         }
+                     }
+                 }
+            }
+    }
+
+    fun logOut() {
+        fbAuth.signOut()
     }
 }
